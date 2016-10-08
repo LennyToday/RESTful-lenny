60f6e9a490c7e43526e591903dd5b3ea5f335ed6
ur dad <thewalk@casperoakley.com>
2016-10-08 11:09:13 +0000

    |■‸■|

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
+server.get('/api/v1/lenny', function(req, res, next)
+{
+	console.log("Sending Default Lenny");
+	
+	var response = {
+		"face": "( ͡° ͜ʖ ͡°)"
+	}
+	res.send(response);
+	return next();
+});
+
+
+
+
+
 server.listen(parseInt(process.argv[2]), function() {
 	  console.log('%s listening at %s', server.name, server.url);
 });
