bd16aebbb01a59e2ed3da1fe98ee05e207eb8ae6
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-12-01 23:59:03 +0000

    ᕕ(⫑⏏⫒)ᕗ AzureFunctions: Adds basics

---
+++ v1-lenny/index.js
@@ -0,0 +1,11 @@
+module.exports = function (context, req) {
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        headers: { 
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: [ { face: "( ͡° ͜ʖ ͡°)" } ]
+    };
+
+    context.done();
+};
