cf0ecedb320668cfb115db92f36cb166dcd2e43a
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-12-11 21:51:28 +0000

    AzureFunctions: Implements v1 random

--- v1-random/index.js
+++ v1-random/index.js
@@ -3,6 +3,11 @@ var gen           = require('random-seed'),
     lennyFactory  = require('./lennyFactory'),
     errors        = require('./errors');
 
+function getRandom(arr, seed) {
+    var rand = gen.create(seed);
+    return arr[Math.floor(rand.random() * arr.length)];
+}
+
 module.exports = function (context, req)
 {
     var limit = req.query.limit;
@@ -17,96 +22,79 @@ module.exports = function (context, req)
             headers: { 
                 "Content-Type": "application/json; charset=utf-8"
             },
-            body: errors.fromCode(400)
+            body: errors.fromCode(400),
+            isRaw: true
         };
 
         context.done();
-        return;
     }
 
-    /*
-    if(!req.query.limit || parseInt(req.query.limit) < 0){
-    req.query.limit = 1;
-  } else {
-    req.query.limit = parseInt(req.query.limit);
-  }
-
-  if(parseInt(req.query.limit) > 500 ||
-     parseInt(req.query.limit) < 0 || 
-     (req.query.lefteye && req.query.lefteye.length > 16) || 
-     (req.query.leftear && req.query.leftear.length > 16) || 
-     (req.query.righteye && req.query.righteye.length > 16) || 
-     (req.query.rightear && req.query.rightear.length > 16) || 
-     (req.query.ears && req.query.ears.length > 16) || 
-     (req.query.eyes && req.query.eyes.length > 16) || 
-     (req.query.mouth && req.query.mouth.length > 16)){
-    res.status(400);
-    res.json(errors.fromCode(400), {'content-type': 'application/json; charset=utf-8'})
-    next();
-  }
-
-  var lennies = [];
-
-  for(var i=0;i<req.query.limit;i++){
-
-
-    var seed =  Math.floor((Math.random() * 4294967294));
-
-    //Give seeds different values to ensure randomness
-    var ear = getRandom(lenny.ears.all, seed),
-        eye = getRandom(lenny.eyes.all, seed+1),
-      mouth = getRandom(lenny.mouths.all, seed+2);
-  
-  
-    var lennyface = { 
-      lefteye: "",  
-      righteye: "",
-      leftear: "",
-      rightear: "",
-      mouth: ""
-    };    
-
-    if(ear.length == 1) {
-     lennyface.leftear = ear[0], lennyface.rightear = ear[0];
-    } else {
-     lennyface.leftear = ear[0], lennyface.rightear = ear[1];
-    }
-
-    if(eye.length == 1) {
-       lennyface.lefteye = eye[0], lennyface.righteye = eye[0];
-    } else {
-      lennyface.lefteye = eye[0], lennyface.righteye = eye[1];
-    }
-    
-    lennyface.mouth = mouth;
-    lennyface = lennyFactory.create(req.query, lennyface);
-
-    var resp = {
-      seed: seed,
-      face: lennyface.leftear + lennyface.lefteye + lennyface.mouth + lennyface.righteye + lennyface.rightear
-    };
-   
-    lennies.push(resp);
-  } 
-    
-
-    if(req.query.test == "break"){
+    if((req.query.lefteye && req.query.lefteye.length > 16) || 
+        (req.query.leftear && req.query.leftear.length > 16) || 
+        (req.query.righteye && req.query.righteye.length > 16) || 
+        (req.query.rightear && req.query.rightear.length > 16) || 
+        (req.query.ears && req.query.ears.length > 16) || 
+        (req.query.eyes && req.query.eyes.length > 16) || 
+        (req.query.mouth && req.query.mouth.length > 16))
+    {
         context.res = {
             status: 400,
             headers: { 
                 "Content-Type": "application/json; charset=utf-8"
             },
-            body: {'ლ(⏓益⏓ლ)':'┬─┬ノ( ´ᗝ`ノ)'}
+            body: errors.fromCode(400),
+            isRaw: true
         };
 
         context.done();
     }
-    */
+
+    var lennies = [];
+
+    for(var i = 0; i < limit; i++)
+    {
+        var seed =  Math.floor((Math.random() * 4294967294));
+
+        //Give seeds different values to ensure randomness
+        var ear = getRandom(lenny.ears.all, seed),
+            eye = getRandom(lenny.eyes.all, seed+1),
+            mouth = getRandom(lenny.mouths.all, seed+2);
+
+
+        var lennyface = { 
+            lefteye: "",  
+            righteye: "",
+            leftear: "",
+            rightear: "",
+            mouth: ""
+        };    
+
+        if(ear.length == 1)
+            lennyface.leftear = ear[0], lennyface.rightear = ear[0];
+        else
+            lennyface.leftear = ear[0], lennyface.rightear = ear[1];
+
+        if(eye.length == 1)
+            lennyface.lefteye = eye[0], lennyface.righteye = eye[0];
+        else
+            lennyface.lefteye = eye[0], lennyface.righteye = eye[1];
+
+        lennyface.mouth = mouth;
+        lennyface = lennyFactory.create(req.query, lennyface);
+
+        var resp = {
+            seed: seed,
+            face: lennyface.leftear + lennyface.lefteye + lennyface.mouth + lennyface.righteye + lennyface.rightear
+        };
+
+        lennies.push(resp);
+    }
+
     context.res = {
         headers: { 
             "Content-Type": "application/json; charset=utf-8"
         },
-        body: [ { face: "( ͡° ͜ʖ ͡°)" }, { limit: limit } ]
+        body: { lennies: lennies, limit: limit }
     };
 
     context.done();
